8460f042c234439a65c6d4f9f065c534a2874091
kangtongyong <kangtongyong54@gmail.com>
2025-11-29 20:00:16 +0900

    25-1-29 로그인 기능 ui, login, menu 추가

--- book_management_project/login.c
+++ book_management_project/login.c
@@ -4,104 +4,61 @@
 #include <stdlib.h>
 #include "login.h"
 #include "structs.h"
+#include "database.h"
+#include "assistant.h"
 
 
-//	로그인 관련 함수
+//  로그인 관련 함수
 
 
-//	로그인 함수
-int testlogin(void)
+/*
+    함수명   :   testlogin()
+    기능 :  DisplayLogin()에서 전달받은 ID, PW를 DB에서 검사,
+            로그인 성공 여부 및 계정 타입/에러 코드를 반환하는 함수
+    매개변수 :  const char* id  - 입력받은 아이디
+                const char* pw  - 입력받은 비밀번호
+    반환값:     ACCOUNT_TYPE_ADMIN (1)      관리자 로그인 성공
+                ACCOUNT_TYPE_USER  (0)      사용자 로그인 성공
+                DB_FILE_NOT_FOUND  (-1)     DB 파일을 열 수 없음
+                DB_RECORD_NOT_FOUND(-4)     로그인 실패
+*/
+int testlogin(const char* id, const char* pw)
 {
+    int usercount = 0;       // 실제로 로드된 사용자 수
 
-	FILE* file;
-	file = fopen("user.txt", "r");
+    //  user.txt 에서 로그인 정보 로드
+    DBERROR err = UserDatabaseLoad(userCount, &usercount);
+    if (err != DB_SUCCESS || usercount <= 0)
+    {
+        // 파일이 없거나, 유저 데이터가 없는 경우
+        return DB_FILE_NOT_FOUND;           //  -1 반환
+    }
 
-	LOGIN logininfo[MAX_USER_COUNT];
+    //  ID / PW 비교
+    for (int i = 0; i < usercount; i++)
+    {
+        if (strcmp(id, logins[i].loginID) == 0 &&
+            strcmp(pw, logins[i].loginPW) == 0)
+        {
+            MoveCursor(31, 24);
+            printf("                                                    ");
+            MoveCursor(31, 24);
 
-	int usercount = 0;
+            //  관리자
+            if (logins[i].accountType == ACCOUNT_TYPE_ADMIN)
+            {
+                printf("\n관리자 %s님 환영합니다.\n", logins[i].loginID);
+                return logins[i].accountType;    // 1 반환
+            }
+            //  사용자
+            else
+            {
+                printf("\n사용자 %s님 환영합니다.\n", logins[i].loginID);
+                return logins[i].accountType;    // 0 반환
+            }
+        }
+    }
 
-	//	파일에서 로그인 정보 읽기
-
-	if (file == NULL)
-	{
-		printf("존재 하지않는 파일입니다.\n");
-		return DB_FILE_NOT_FOUND;
-	}
-
-	while (fscanf(file, "%d %28s %48s",
-		&logininfo[usercount].accountType,
-		logininfo[usercount].loginID,
-		logininfo[usercount].loginPW) != EOF)
-	{
-		usercount++;
-	}
-
-	fclose(file);
-
-
-	//	id와 pw 저장 변수
-	char id[MAX_USER_ID_LENGTH];
-	char pw[MAX_USER_PASSWORD_LENGHTH];
-
-	//	잘못된 로그인 시도 저장 
-	int wrongcount = 0;
-
-
-	//	무한루프를 돌면서 아이디와 비밀번호를 입력받고 확인
-	while (1)
-	{
-		// 일정 시도 초과
-		if (WRONGCOUNT_FAIL <= wrongcount)
-		{
-			printf("\n너무 많은 잘못된 로그인 시도입니다.\n프로그램을 종료합니다.\n");
-			return -1;
-		}
-		else
-		{
-			if (wrongcount < 3)
-			{
-				//	아이디와 비밀번호 입력
-				printf("\n아이디와 비밀번호를 입력하세요.\n");
-			}
-			else
-			{
-				printf("\n--%d회 이상 로그인을 실패하셨습니다.--\n10회 이상 로그인 실패시 로그인이 금지됩니다.\n", wrongcount);
-			}
-
-			printf("ID:");
-			scanf(" %28s", id);		//	공백 문자 건너뛰기 
-
-			printf("Password:");
-			scanf(" %48s", pw);		//	공백 문자 건너뛰기 
-		}
-
-		//	반복문을 통한 비교
-		for (int i = 0; i < usercount; i++)
-		{
-
-			//	아이디, 비밀번호 확인
-			if (strcmp(id, logininfo[i].loginID) == 0 && strcmp(pw, logininfo[i].loginPW) == 0)
-			{
-				//	관리자
-				if (logininfo[i].accountType == ACCOUNT_TYPE_ADMIN)
-				{
-					printf("\n관리자 %s님 환영합니다.\n", logininfo[i].loginID);
-					wrongcount = 0;
-					return logininfo[i].accountType;
-				}
-
-				//	사용자
-				else
-				{
-					printf("\n사용자 %s님 환영합니다.\n", logininfo[i].loginID);
-					wrongcount = 0;
-					return logininfo[i].accountType;
-				}
-			}
-		}
-
-		//	로그인 실패
-		printf("로그인 실패 : 아이디 또는 비밀번호가 잘못되었습니다.\n\n");
-		wrongcount++;
-	}
+    //  로그인 실패
+    return DB_RECORD_NOT_FOUND;                 // -4
 }
